39fad7d156ec2ac273ec19ed3251887498f6bb35
Yashil Lal Singh <yashilsingh0208@gmail.com>
2025-02-25 14:47:05 +0545

    updated numbers

--- types/numbers.js
+++ types/numbers.js
@@ -18,6 +18,30 @@ console.log(infinity);
 
 // Number Methods
 
-// isNan
+// isNaN() - Checks if given expression is not a number
 console.log(isNaN(infinity)); // false
 console.log(isNaN("Hello")); // true
+
+// parseFloat() - Converts to floating point number
+console.log(parseFloat("11"));
+
+// parseInt() - Converts integer
+console.log(parseInt("11.1")); // 11
+
+// toFixed() - Returns a string value for a number in fixed-point notation
+console.log(decimalNumber.toFixed(1)); // 11.1
+
+// toPresision() -
+console.log(decimalNumber.toPrecision(3)); // 11.1
+
+// toString() - Coverts to string
+console.log(decimalNumber.toString()); // 11.11 (string)
+
+// toLocaleString() - Returns language-sensitive representation of the given number.
+let num = 400000;
+console.log(
+  num.toLocaleString("en-IN", {
+    style: "currency",
+    currency: "NPR",
+  })
+); // NPR 4,00,000
